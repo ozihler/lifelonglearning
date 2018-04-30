77c86524a91ea970bafe7950540e0fbc0d3612a8
Oliver Zihler <oliver.zihler@zuehlke.com>
2018-04-30 13:25:20 +0200

    initial commit

---
+++ src/main/java/com/zuehlke/app/lifelonglearning/LifelonglearningApplication.java
@@ -0,0 +1,12 @@
+package com.zuehlke.app.lifelonglearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifelonglearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifelonglearningApplication.class, args);
+	}
+}
